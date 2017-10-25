8835023068ee108f688bc411de6eb7c70721d32d
Lung <lung@skaut.cz>
2017-10-25 21:17:23 +0200

    added PHPmailer

--- src/routes.php
+++ src/routes.php
@@ -6,10 +6,10 @@ use Slim\Http\Response;
 // Routes
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+	// Sample log message
+	$this->logger->info("Slim-Skeleton '/' route");
+	
+	// Render index view
+	return $this->renderer->render($response, 'index.phtml', $args);
 });
 
