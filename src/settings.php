a2f432b4cb2c7a08421b38d4cf6a57c469330e0f
Martin Pecka <peckama2@fel.cvut.cz>
2017-10-25 22:00:19 +0200

    Implemented basic set of routes. Not doing much so far (signup works).

--- src/settings.php
+++ src/settings.php
@@ -3,34 +3,36 @@ return [
 	'settings' => [
 		'displayErrorDetails' => true, // set to false in production
 		'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-		
+
 		// Renderer settings
 		'renderer' => [
 			'template_path' => __DIR__.'/../templates/',
 		],
-		
+
 		// Monolog settings
 		'logger' => [
 			'name' => 'slim-app',
 			'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__.'/../logs/app.log',
 			'level' => \Monolog\Logger::DEBUG,
 		],
-		
+
 		// PHPmailer settings
 		'mailer' => [
 			'smtp_server' => '',
 			'smtp_username' => '',
 			'smtp_password' => '',
-			
+
 			'from_mail' => '', //registration mail
 			'from_name' => '',
-			
+
 			'bcc_mail' => '', //registration mail for example
 			'bcc_name' => '',
 		],
 
 		'db' => [
 			'path' => __DIR__ . '/../db'
-		]
+		],
+
+        'eventName' => 'cej2018',
 	],
 ];
