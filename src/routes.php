bfdc5d1cc78a3bc00512192c5887b3e19fa022be
Lung <lung@skaut.cz>
2017-10-25 20:16:29 +0200

    edited README and permissions

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,4 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
