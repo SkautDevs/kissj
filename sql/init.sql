61c6722c5e84d6c9f490cc7f08ef2d4e0b37d118
Lung <lung@skaut.cz>
2017-11-22 22:48:36 +0100

    fixed db

--- sql/init.sql
+++ sql/init.sql
@@ -1,80 +1,80 @@
 CREATE TABLE user
 (
-	id                  INTEGER	PRIMARY KEY	autoincrement,
-	email               TEXT NOT NULL,
-  role_id             INT CONSTRAINT user_role_id_fk REFERENCES role (id)
+	id      INTEGER PRIMARY KEY    autoincrement,
+	email   TEXT NOT NULL,
+	role_id INT CONSTRAINT user_roleId_fk REFERENCES role (id)
 );
 
 CREATE TABLE patrolparticipant
 (
-	id                   INTEGER PRIMARY KEY autoincrement,
-	userId              INT CONSTRAINT participant_patrolleader_id_fk REFERENCES patrolleader (id),
-	firstName           TEXT,
-	lastName            TEXT,
-	nationality          TEXT,
-	gender               TEXT,
-	address              TEXT,
-	phone                TEXT,
-	email                TEXT,
-	scoutUnit           TEXT,
-	birthDate           DATETIME,
-	birthPlace          TEXT,
-	allergies            TEXT,
-	foodPreferences     TEXT,
+	id                 INTEGER PRIMARY KEY autoincrement,
+	userId             INT CONSTRAINT participant_patrolleaderId_fk REFERENCES patrolleader (id),
+	firstName          TEXT,
+	lastName           TEXT,
+	nationality        TEXT,
+	gender             TEXT,
+	address            TEXT,
+	phone              TEXT,
+	email              TEXT,
+	scoutUnit          TEXT,
+	birthDate          DATETIME,
+	birthPlace         TEXT,
+	allergies          TEXT,
+	foodPreferences    TEXT,
 	cardPassportNumber TEXT,
-	notes                TEXT
+	notes              TEXT
 );
 
 CREATE TABLE patrolleader
 (
-	id                   INTEGER PRIMARY KEY autoincrement,
-	userId              INT CONSTRAINT patrolleader_user_id_fk REFERENCES USER (id),
-	finished             BOOLEAN,
-	patrolName          TEXT,
+	id                 INTEGER PRIMARY KEY autoincrement,
+	userId             INT CONSTRAINT patrolleader_userId_fk REFERENCES USER (id),
+	finished           BOOLEAN,
+	patrolName         TEXT,
 	-- same as patrolparticipant
-	firstName           TEXT,
-	lastName            TEXT,
-	nationality          TEXT,
-	gender               TEXT,
-	address              TEXT,
-	phone                TEXT,
-	email                TEXT,
-	scoutUnit           TEXT,
-	birthDate           DATETIME,
-	birthPlace          TEXT,
-	allergies            TEXT,
-	foodPreferences     TEXT,
+	firstName          TEXT,
+	lastName           TEXT,
+	nationality        TEXT,
+	gender             TEXT,
+	address            TEXT,
+	phone              TEXT,
+	email              TEXT,
+	scoutUnit          TEXT,
+	birthDate          DATETIME,
+	birthPlace         TEXT,
+	allergies          TEXT,
+	foodPreferences    TEXT,
 	cardPassportNumber TEXT,
-	notes                TEXT
+	notes              TEXT
 
 );
 
 CREATE TABLE ist
 (
-	id                     INTEGER PRIMARY KEY autoincrement,
-	userId                INT CONSTRAINT ist_user_id_fk REFERENCES USER (id),
-	finished               BOOLEAN,
-	workPreferences       TEXT,
-	skills                 TEXT,
-	languages              TEXT,
-	arrivalDate           DATETIME,
-	leavingDate           DATETIME,
+	id                   INTEGER PRIMARY KEY autoincrement,
+	userId               INT CONSTRAINT ist_userId_fk REFERENCES USER (id),
+	finished             BOOLEAN,
+	workPreferences      TEXT,
+	skills               TEXT,
+	languages            TEXT,
+	arrivalDate          DATETIME,
+	leavingDate          DATETIME,
 	carRegistrationPlate TEXT,
 	-- same as patrolparticipant
-	firstName             TEXT,
-	lastName              TEXT,
-	nationality            TEXT,
-	gender                 TEXT,
-	address                TEXT,
-	phone                  TEXT,
-	email                  TEXT,
-	scoutUnit             TEXT,
-	birthDate             DATETIME,
-	birthPlace            TEXT,
-	allergies              TEXT,
-	foodPreferences       TEXT,
+	firstName            TEXT,
+	lastName             TEXT,
+	nationality          TEXT,
+	gender               TEXT,
+	address              TEXT,
+	phone                TEXT,
+	email                TEXT,
+	scoutUnit            TEXT,
+	birthDate            DATETIME,
+	birthPlace           TEXT,
+	allergies            TEXT,
+	foodPreferences      TEXT,
 	cardPassportNumber   TEXT,
-	notes                  TEXT
+	notes                TEXT
 
 );
 
@@ -87,7 +87,7 @@ CREATE TABLE logintoken
 		PRIMARY KEY
 	autoincrement,
 	token   TEXT NOT NULL,
-	user_id INT
+	userId  INT
 	CONSTRAINT login_tokens_users_id_fk
 	REFERENCES USER (id),
 	created DATETIME,
@@ -96,7 +96,7 @@ CREATE TABLE logintoken
 
 CREATE TABLE role
 (
-  id                     INTEGER PRIMARY KEY autoincrement,
-  name                   TEXT
+	id   INTEGER PRIMARY KEY autoincrement,
+	name TEXT
 
 );
