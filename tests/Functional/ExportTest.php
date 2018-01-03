32774339f0cde903a89c730b61a0aba47168e2b1
racinmat <racinmat@fel.cvut.cz>
2018-01-03 22:11:29 +0100

    Added tests, and working export form db to array.

--- tests/Functional/ExportTest.php
+++ tests/Functional/ExportTest.php
@@ -4,6 +4,8 @@ namespace Tests\Functional;
 
 
 use kissj\Export\ExportService;
+use kissj\Participant\Patrol\PatrolService;
+use kissj\User\RoleService;
 use kissj\User\UserService;
 
 class ExportTest extends BaseTestCase {
@@ -15,13 +17,44 @@ class ExportTest extends BaseTestCase {
 		$app = $this->app();
 		/** @var ExportService $exportService */
 		$exportService = $app->getContainer()->get('exportService');
+		/** @var UserService $userService */
+		$userService = $app->getContainer()->get('userService');
+		/** @var PatrolService $patrolService */
+		$patrolService = $app->getContainer()->get('patrolService');
+		/** @var RoleService $roleService */
+		$roleService = $app->getContainer()->get('roleService');
 
+		for ($i = 0; $i < 10; $i++) {
+			$email = 'test-'. $i . '@example.com';
+			$user = $userService->registerUser($email);
+			$patrolLeader = $patrolService->getPatrolLeader($user);
+			$patrolService->editPatrolLeaderInfo($patrolLeader,
+				'leader',
+				'leaderový',
+				'burákové máslo' . $i,
+				(new \DateTime())->format(DATE_ISO8601),
+				'Kalimdor',
+				'Azeroth',
+				'attack helicopter',
+				'Northrend',
+				'High Elves',
+				'none',
+				'test'.$i.'@test.moe',
+				'trolls',
+				'some',
+				'some note',
+				'my great patrol'
+			);
+		}
+
+		$rows = $exportService->medicalDataToCSV('cej2018');
 //		$email = 'test@example.com';
 //		$user = $userService->registerUser($email);
 //		$readableRole = 'Patrol Leader';
 //		$token = $userService->sendLoginTokenByMail($email, $readableRole);
 //		$loadedUser = $userService->getUserFromToken($token);
 
-		$this->assertTrue(false);
+
+		$this->assertCount(11, $rows);
 	}
 }
