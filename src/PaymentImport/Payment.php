63bf41a17bf2445748a3166467943d00a483fce5
Martin Pecka <peckama2@fel.cvut.cz>
2018-01-03 21:35:02 +0100

    Started money import support.

---
+++ src/PaymentImport/Payment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace kissj\PaymentImport;
+
+class Payment {
+    public $event; /** @var string */
+    public $variableSymbol; /** @var int */
+    public $senderName; /** @var string */
+    public $senderAccountNr; /** @var string */
+    public $amount; /** @var float */
+    public $currency; /** @var string */
+    public $noteForReceiver; /** @var string */
+    public $dateReceived; /** @var \DateTimeImmutable */
+}
