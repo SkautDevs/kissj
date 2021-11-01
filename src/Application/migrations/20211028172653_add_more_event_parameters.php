503b56c98d6b49a0465b26bdea3e7b43bdea214a
Michael Kaplan <mkaplan@redhat.com>
2021-11-01 18:35:43 +0100

    feat/db: refactor migrations

--- src/Application/migrations/20211028172653_add_more_event_parameters.php
+++ src/Application/migrations/20211028172653_add_more_event_parameters.php
@@ -7,55 +7,20 @@ use Phinx\Migration\AbstractMigration;
 final class AddMoreEventParameters extends AbstractMigration
 {
     public function up(): void
-    {
-        $this->query('ALTER TABLE `event`
-	        ADD `default_price` INTEGER NOT NULL DEFAULT 0;');
-        
-        $this->query('ALTER TABLE `event`
-	        ADD `event_type` TEXT DEFAULT "default" NOT NULL;');
-        
-        $this->query('ALTER TABLE `event`
-	        ADD `logo_url` TEXT DEFAULT "" NOT NULL;');
-    }
+	{
+		$event = $this->table('event');
+		$event->addColumn('default_price', 'integer', ['default' => '0'])
+			->addColumn('event_type', 'text', ['default' => 'default'])
+			->addColumn('logo_url', 'text', ['default' => ''])
+			->save();
+	}
 
     public function down(): void
     {
-        $this->query('CREATE TABLE `event_dg_tmp`
-    (
-        id INTEGER
-		primary key autoincrement,
-	slug TEXT,
-	readable_name TEXT,
-	web_url TEXT,
-	data_protection_url INT,
-	contact_email TEXT,
-	account_number TEXT,
-	prefix_variable_symbol INTEGER,
-	automatic_payment_pairing INTEGER,
-	bank_id INTEGER,
-	bank_api_key TEXT,
-	max_elapsed_payment_days INTEGER,
-	currency TEXT,
-	allow_patrols INTEGER,
-	maximal_closed_patrols_count INTEGER,
-	minimal_patrol_participants_count INTEGER,
-	maximal_patrol_participants_count INTEGER,
-	allow_ists INTEGER,
-	maximal_closed_ists_count INTEGER,
-	allow_guests INTEGER,
-	maximal_closed_guests_count INTEGER,
-	start_day DATE,
-	end_day DATE,
-	created_at DATETIME,
-	updated_at DATETIME,
-	email_from TEXT default "example@example.com" not null,
-	email_from_name TEXT default "Registration Ofiice" not null
-);');
-
-        $this->query('INSERT INTO event_dg_tmp(id, slug, readable_name, web_url, data_protection_url, contact_email, account_number, prefix_variable_symbol, automatic_payment_pairing, bank_id, bank_api_key, max_elapsed_payment_days, currency, allow_patrols, maximal_closed_patrols_count, minimal_patrol_participants_count, maximal_patrol_participants_count, allow_ists, maximal_closed_ists_count, allow_guests, maximal_closed_guests_count, start_day, end_day, created_at, updated_at, email_from, email_from_name) select id, slug, readable_name, web_url, data_protection_url, contact_email, account_number, prefix_variable_symbol, automatic_payment_pairing, bank_id, bank_api_key, max_elapsed_payment_days, currency, allow_patrols, maximal_closed_patrols_count, minimal_patrol_participants_count, maximal_patrol_participants_count, allow_ists, maximal_closed_ists_count, allow_guests, maximal_closed_guests_count, start_day, end_day, created_at, updated_at, email_from, email_from_name FROM `event`;');
-
-        $this->query('DROP TABLE `event`;');
-
-        $this->query('ALTER TABLE `event_dg_tmp` RENAME TO `event`;');
+		$event = $this->table('event');
+		$event->removeColumn('default_price')
+			->removeColumn('event_type')
+			->removeColumn('logo_url')
+			->save();
     }
 }
--- src/Settings/phinxConfiguration.php
+++ src/Settings/phinxConfiguration.php
@@ -10,14 +10,22 @@ return [
         'migrations' => '%%PHINX_CONFIG_DIR%%/../Application/migrations',
         'seeds' => '%%PHINX_CONFIG_DIR%%/../Application/migrations/seeds',
     ],
-    'environments' => [
+	'environments' => [
         'default_migration_table' => 'phinx_log',
-        'default_environment' => 'dev',
-        'dev' => [
+        'default_environment' => $_ENV['DB_TYPE'],
+        'sqlite' => [
             'adapter' => 'sqlite',
             'name' => __DIR__.'/../db_dev',
             'suffix' => '.sqlite',
             'charset' => 'utf8',
         ],
-    ],
+		'postgresql' =>[
+			'adapter' => 'pgsql',
+			'host' => $_ENV['DATABASE_HOST'],
+			'name' => $_ENV['POSTGRES_DB'],
+			'user' => $_ENV['POSTGRES_USER'],
+			'pass' => $_ENV['POSTGRES_PASSWORD'],
+		],
+	],
 ];
+
