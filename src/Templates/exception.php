a28cfc365ae8da9a47245fb261ed1eeb2a9b1e88
Vojtech Mares <iam@vojtechmares.com>
2021-12-04 18:55:53 +0100

    refactor: error 500 page

--- src/Templates/exception.php
+++ src/Templates/exception.php
@@ -2,16 +2,57 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Exception</title>
+	<meta name="robots" content="noindex">
+	<title>KISSJ | Internal Server Error</title>
+	<style>
+		:root {
+			font-family: 'Helvetica', 'Arial', sans-serif;
+			font-size: 1.5rem;
+		}
+		body {
+			background-color: #000;
+			color: #fff;
+		}
+		.error-container {
+			display: block;
+			margin: 0 auto;
+			max-width: 36rem;
+			margin-top: 18rem;
+		}
+
+		.error-container > h1 {
+			text-align: center;
+			font-family: 'Courier New', Courier, monospace;
+			font-size: 2.5rem;
+			margin-bottom: 1rem;
+			font-weight: bold;
+		}
+
+		.error-container > h2 {
+			text-align: center;
+		}
+
+		.error-container > p {
+			margin: 0 4rem;
+		}
+
+		.error-container > p.error-code {
+			font-family: 'Courier New', Courier, monospace;
+			font-size: .8rem;
+		}
+	</style>
 </head>
-<body style="background: black; color: white">
-<h1>KISSJ is broken )-:</h1>
-<p>We're sorry that something went wrong. We know about the problem and are working on solution. </p>
-<p>In the meantime, you may try to:</p>
-<ul>
-	<li>refresh the page by pressing <b>F5</b> or <b>Ctrl + R</b>,</li>
-	<li>close this window, then open main registration page again,</li>
-	<li>wait a few minutes.</li>
-</ul>
+<body>
+<div class="error-container">
+	<h1>Internal Server Error</h1>
+	<h2>Oops, this is embarrassing...</h2>
+	<p>
+		We are sorry, you did nothing wrong, this is our bad. We have been notified and we are working on a fix.<br/>
+		<br/>
+		Please try again in a few minutes.
+	</p>
+	<br/>
+	<p class="error-code">/: error 500 :/</p>
+</div>
 </body>
 </html>
