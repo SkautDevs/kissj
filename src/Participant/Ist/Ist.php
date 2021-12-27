f2bf460ee483022fdde73fbdf9f9683b568e804d
Lung <lung@skaut.cz>
2021-12-27 18:38:32 +0100

    fixed leanmapper definition

--- src/Participant/Ist/Ist.php
+++ src/Participant/Ist/Ist.php
@@ -6,7 +6,7 @@ use kissj\Participant\Participant;
 
 /**
  * @property string|null $skills
- * @property string[] $preferredPosition m:useMethods(getPreferredPosition|setPreferredPosition)
+ * @property array $preferredPosition m:useMethods(getPreferredPosition|setPreferredPosition)
  * @property string|null $driversLicense
  */
 class Ist extends Participant
