ab18775e2024ed8396c24cfaecba6e2ce84e0825
Vojtech Mares <iam@vojtechmares.com>
2022-03-09 19:50:33 +0100

    refactor(Wsj): add custom requirements by CZ contingent

--- src/Event/EventType/Wsj/EventTypeWsj.php
+++ src/Event/EventType/Wsj/EventTypeWsj.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace kissj\Event\EventType\Wsj;
 
+use kissj\Event\ContentArbiterIst;
+use kissj\Event\ContentArbiterPatrolLeader;
+use kissj\Event\ContentArbiterPatrolParticipant;
 use kissj\Event\EventType\EventType;
 
 class EventTypeWsj extends EventType
@@ -18,13 +21,62 @@ class EventTypeWsj extends EventType
         ];
     }
 
+    public function getContentArbiterPatrolLeader(): ContentArbiterPatrolLeader
+    {
+        $ca = parent::getContentArbiterPatrolLeader();
+
+        $ca->patrolName = false;
+        $ca->idNumber = true;
+        $ca->languages = true;
+        $ca->food = true;
+        $ca->phone = true;
+        $ca->email = true;
+        $ca->swimming = true;
+        $ca->unit = true;
+        $ca->skills = true;
+
+        return $ca;
+    }
+
+    public function getContentArbiterPatrolParticipant(): ContentArbiterPatrolParticipant
+    {
+        $ca = parent::getContentArbiterPatrolParticipant();
+
+        $ca->idNumber = true;
+        $ca->languages = true;
+        $ca->food = true;
+        $ca->phone = true;
+        $ca->email = true;
+        $ca->swimming = true;
+        $ca->unit = true;
+
+        return $ca;
+    }
+
+    public function getContentArbiterIst(): ContentArbiterIst
+    {
+        $ca = parent::getContentArbiterIst();
+
+        $ca->unit = true;
+        $ca->phone = true;
+        $ca->email = true;
+        $ca->food = true;
+        $ca->skills = true;
+        $ca->preferredPosition = true;
+        $ca->idNumber = true;
+        $ca->languages = true;
+        $ca->swimming = true;
+
+        return $ca;
+    }
+
     /**
      * @return array<string, string>
      */
     public function getLanguages(): array
     {
         return [
-            'en' => '🇬🇧 English',
+            'cs' => '🇨🇿 Česky',
         ];
     }
 }
