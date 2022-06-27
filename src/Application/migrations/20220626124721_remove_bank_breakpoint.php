15e0f57976931c9e4a3943ac13ec7f10ecdd8349
Lung <lung@skaut.cz>
2022-06-27 21:58:51 +0200

    fixed migration

--- src/Application/migrations/20220626124721_remove_bank_breakpoint.php
+++ src/Application/migrations/20220626124721_remove_bank_breakpoint.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class AddBankBreakpoint extends AbstractMigration
+final class RemoveBankBreakpoint extends AbstractMigration
 {
     public function up(): void
     {
