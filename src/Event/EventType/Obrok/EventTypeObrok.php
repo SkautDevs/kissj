a8a97db70b7a20071fa46c4d55269bd4de201ceb
paaton <paatzondrej@seznam.cz>
2023-11-29 16:13:26 +0100

    commit of requested changes vol. 2

--- src/Event/EventType/Obrok/EventTypeObrok.php
+++ src/Event/EventType/Obrok/EventTypeObrok.php
@@ -27,6 +27,7 @@ class EventTypeObrok extends EventType
     {
         $ca = parent::getContentArbiterIst();
         $ca->tshirt = true;
+        $ca->medicaments = true;
         $ca->printedHandbook = true;
         return $ca;
     }
@@ -38,6 +39,7 @@ class EventTypeObrok extends EventType
         $ca->phone = true;
         $ca->email = true;
         $ca->unit = true;
+        $ca->medicaments = true;
         $ca->printedHandbook = true;
 
         return $ca;
@@ -50,6 +52,7 @@ class EventTypeObrok extends EventType
         $ca->phone = true;
         $ca->email = true;
         $ca->unit = true;
+        $ca->medicaments = true;
         $ca->printedHandbook = true;
 
         return $ca;
