135b9d80bff2f67e76b3fe699ba513beb846fe7a
Michael Kaplan <michael@kaplan.sh>
2024-02-23 08:20:01 +0100

    fix: gracefully fallback to local sessions

--- src/Session/RedisSessionHandler.php
+++ src/Session/RedisSessionHandler.php
@@ -11,14 +11,13 @@ class RedisSessionHandler implements SessionHandlerInterface
 {
     public function __construct(
         readonly private Redis $redis,
-        readonly string $address,
-        readonly int $port,
         readonly string $host,
+        readonly int $port,
         readonly string $password,
         readonly private int $ttl = 60 * 60 * 24 * 7, // one week in seconds
     ) {
-        $this->redis->connect($address, $port);
-        $this->redis->auth([$host => $password]);
+        $this->redis->connect($host, $port);
+        $this->redis->auth(['pass' => $password]);
     }
 
     public function open(string $path, string $name): bool
