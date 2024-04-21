1a6740253601e25fca805c0989fd448a373c9963
Lung <lung@skaut.cz>
2024-04-21 14:55:21 +0200

    done all admin methods working with payments event-aware

--- src/Payment/PaymentRepository.php
+++ src/Payment/PaymentRepository.php
@@ -4,18 +4,33 @@ declare(strict_types=1);
 
 namespace kissj\Payment;
 
+use Dibi\Row;
 use kissj\Event\Event;
-use kissj\Orm\Order;
 use kissj\Orm\Repository;
+use RuntimeException;
 
-/**
- * @method Payment get(int $paymentId)
- * @method Payment getOneBy(mixed[] $criteria)
- * @method Payment[] findBy(mixed[] $criteria, Order[] $orders = [])
- * @method Payment|null findOneBy(mixed[] $criteria, Order[] $orders = [])
- */
 class PaymentRepository extends Repository
 {
+    public function getById(int $paymentId, Event $event): Payment
+    {
+        $qb = $this->createFluent();
+        $qb->join('participant')->as('participant')->on('participant.id = payment.participant_id');
+        $qb->join('user')->as('u')->on('u.id = participant.user_id');
+        $qb->where('u.event_id = %i', $event->id);
+        $qb->where('payment.id = %i', $paymentId);
+
+        /** @var ?Row $row */
+        $row = $qb->fetch();
+        if ($row === null) {
+            throw new RuntimeException(sprintf('Payment with ID %s not found', $paymentId));
+        }
+
+        /** @var Payment $payment */
+        $payment = $this->createEntity($row);
+
+        return $payment;
+    }
+
     public function isVariableNumberExisting(string $variableNumber): bool
     {
         return $this->isExisting(['variable_symbol' => $variableNumber]);
