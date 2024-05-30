01af873e2299bfd1e106b5f7d7cd220e80abcf8e
Lung <lung@skaut.cz>
2024-05-30 10:30:01 +0200

    added Allow-Health into cors middleware

--- src/Middleware/AddCorsHeaderForAppDomainsMiddleware.php
+++ src/Middleware/AddCorsHeaderForAppDomainsMiddleware.php
@@ -20,7 +20,7 @@ class AddCorsHeaderForAppDomainsMiddleware extends BaseMiddleware
 
         return $response->withHeader('Access-Control-Allow-Origin', 'https://kissj.skauting.cz') // TODO fix for production - make dynamic or list
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->withHeader('Access-Control-Allow-Headers', 'authorization')
+            ->withHeader('Access-Control-Allow-Headers', ['authorization', 'Allow-Health'])
             ->withHeader('Access-Control-Allow-Credentials', 'true'); // also handle cookies
     }
 }
