942d3cd40c8e85bfb14cf94088b94e8ac39440ef
Lung <lung@skaut.cz>
2024-08-08 12:02:12 +0200

    fixed entry numbers for admins

--- src/Participant/ParticipantRepository.php
+++ src/Participant/ParticipantRepository.php
@@ -671,22 +671,19 @@ class ParticipantRepository extends Repository
 
         $qb->where('u.status = %s', UserStatus::Paid);
         $qb->where('u.event_id = %i', $event->id);
+
         return $qb;
     }
 
-    /**
-     * @param Event $event
-     * @return Fluent
-     * @throws \LeanMapper\Exception\InvalidStateException
-     */
     private function getQueryBuilderCountForPaidPatrolParticipants(Event $event): Fluent
     {
         $qb = $this->connection->select('COUNT(participant.id)')->from($this->getTable());
         $qb->join('participant')->as('pl')->on('pl.id = participant.patrol_leader_id');
-        $qb->join('user')->as('u')->on('u.id = participant.user_id');
+        $qb->join('user')->as('u')->on('u.id = pl.user_id');
 
         $qb->where('u.status = %s', UserStatus::Paid);
         $qb->where('u.event_id = %i', $event->id);
+
         return $qb;
     }
 }
