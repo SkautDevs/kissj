b7931861bcaa51910cc7dcc85c8922ca7bf36bc4
Lung <lung@skaut.cz>
2026-02-22 19:11:18 +0100

    added ContentArbiterItemType enum with all field rendering types

---
+++ tests/Unit/Event/ContentArbiter/ContentArbiterItemTypeTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\Event\ContentArbiter;
+
+use kissj\Event\ContentArbiter\ContentArbiterItemType;
+use PHPUnit\Framework\TestCase;
+
+class ContentArbiterItemTypeTest extends TestCase
+{
+    public function testAllExpectedTypesExist(): void
+    {
+        $expectedTypes = [
+            'text', 'email', 'phone', 'date', 'select', 'radio',
+            'textarea', 'file', 'checkbox', 'tshirtComposite',
+        ];
+
+        $actualValues = array_map(fn (ContentArbiterItemType $t) => $t->value, ContentArbiterItemType::cases());
+        sort($expectedTypes);
+        sort($actualValues);
+
+        self::assertSame($expectedTypes, $actualValues);
+    }
+}
