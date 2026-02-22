b10f5ffb3d477fffeaad7570fc8cba2a682bb8bc
Lung <lung@skaut.cz>
2026-02-22 19:11:18 +0100

    added ContentArbiterItemType enum with all field rendering types

---
+++ src/Event/ContentArbiter/ContentArbiterItemType.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace kissj\Event\ContentArbiter;
+
+enum ContentArbiterItemType: string
+{
+    case Text = 'text';
+    case Email = 'email';
+    case Phone = 'phone';
+    case Date = 'date';
+    case Select = 'select';
+    case Radio = 'radio';
+    case Textarea = 'textarea';
+    case File = 'file';
+    case Checkbox = 'checkbox';
+    case TshirtComposite = 'tshirtComposite';
+}
